de242a95852b024e87a623aa727ae09ae850ac50
kushalbhalla7 <kushal.bhalla@appventurez.com>
2022-09-15 18:55:36 +0530

    commit w different port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app)
 
-server.listen(3000, () => {
+server.listen(3001, () => {
     console.log("server is running");
 })
